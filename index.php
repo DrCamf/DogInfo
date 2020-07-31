2cb332365954b8a1f1a823b7c60d4db9f82f65bf
DrCamf <drcamf@gmail.com>
2020-07-31 13:56:23 +0200

    pure javascript vs vue

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+$breed_select = array();
+$breed_select
+https://api.thedogapi.com/v1/breeds
+
+public function PopulateBreed($breeds)
+{
+    
+}
+?>
+
+<!-- HTML code -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Doggo info </title>
+</head>
+<body>
+
+
+
+
+</body>
